f8a7d46ae90499dd92fc6ddea559c9b8b5a3a3c2
Connor Sanderson <connor.sanderson@outlook.com>
2022-03-29 21:36:24 +1100

    completed routes, controllers and basic views

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "Cleaning database..."
+Restaurant.destroy_all
+puts "Creating restaurants..."
+
+10.times do
+  restaurant_types = ["chinese", "italian", "japanese", "french", "belgian"].sample
+  restaurant = Restaurant.new(name: Faker::Restaurant.name, address: Faker::Address.street_address, category: restaurant_types, phone_number: Faker::PhoneNumber.phone_number)
+  puts "Created #{restaurant.name}"
+  restaurant.save
+  puts "saving to the database"
+end
+puts "finished"
